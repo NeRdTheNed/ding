20b748be6bafcddfb9bb604ea4ebb7444f079e35
Ned Loynd <41816363+NeRdTheNed@users.noreply.github.com>
2020-07-14 01:21:16 +1000

    updated fabric-loom, fixed mod
    -added .DS_Store files to gitignore because mac os is annoying
    -switched to fabric loom 0.4 and gradle 6.5
    -updated mappings and mod dependencies to 20w17a as this snapshot broke the mixin. this is compatible with versions up to at least 1.16.1, and will probably remain compatible until rendering changes again.
    -bumped version to 4.0.0
    -fixed the actual mixin by adding one argument

--- src/main/java/com/github/draylar/ding/mixin/RenderMainMenuMixin.java
+++ src/main/java/com/github/draylar/ding/mixin/RenderMainMenuMixin.java
@@ -2,6 +2,7 @@ package com.github.draylar.ding.mixin;
 
 import com.github.draylar.ding.Ding;
 import net.minecraft.client.gui.screen.TitleScreen;
+import net.minecraft.client.util.math.MatrixStack;
 import net.minecraft.util.Identifier;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
@@ -14,7 +15,7 @@ public abstract class RenderMainMenuMixin {
     private static boolean firstTime = true;
 
     @Inject(at = @At("HEAD"), method = "render")
-    private void render(int mouseX, int mouseY, float delta, CallbackInfo info) {
+    private void render(MatrixStack matrices, int mouseX, int mouseY, float delta, CallbackInfo info) {
         if (firstTime) {
             Identifier loadSoundID = new Identifier(Ding.CONFIG.loadSound);
 
